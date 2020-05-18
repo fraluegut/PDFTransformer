8174f9f820db4d9bdf9226f5271c51755d3f3795
fraluegut <franciscojavierluengo@outlook.com>
2020-05-18 19:53:14 +0200

    Fórmula base satisfactoria

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ read_pdf = PyPDF2.PdfFileReader(pdf_file)
 number_of_pages = read_pdf.getNumPages()
 print(number_of_pages)"""
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfFileReader, PdfFileMerger, PdfFileWriter
 import numpy as np
 import glob
 
@@ -31,8 +31,8 @@ def extract_information(pdf_path):
     return information
 
 
-#numero_pg = PdfFileReader(open('Desobediencia_civil.pdf', 'rb')).getNumPages()
-numero_pg = 48
+numero_pg = PdfFileReader(open('Desobediencia_civil.pdf', 'rb')).getNumPages()
+#numero_pg = 40
 print("Número de páginas del pdf: ")
 print(numero_pg)
 
@@ -40,24 +40,7 @@ print(numero_pg)
 print("Número de folios teórico: ")
 print(numero_pg/4)
 ###################################################################
-num2 = 4
-def mcd(num1, num2):
-    a = max(num1, num2)
-    b = min(num1, num2)
-    while b!=0:
-        mcd = b
-        b = a%b
-        a = mcd
-    return mcd
 
-def mcm(num1, num2):
-    a = max(num1, num2)
-    b = min(num1, num2)
-    mcm = (a / mcd(a,b)) * b
-    return mcm
-
-
-###################################################################
 # Redondeo forzado hacia arriba
 import math
 #math.ceil(1.1)
@@ -66,208 +49,60 @@ import math
 numero_folios_reales = math.ceil(numero_pg/4)
 print("Número de folios reales:")
 print(numero_folios_reales)
-mcm_num_pg = mcm(numero_pg,numero_folios_reales)
+
 ###################################################################
 # Matrix con num min de carillas que es el mínimo común múltiplo del número de páginas
 matrix = np.arange(numero_folios_reales*4).reshape((numero_folios_reales,4))
+
+# Matriz base:
+print("Matriz base:")
 print(matrix)
-
-"""matrix[0,0] = int(numero_pg)
-matrix[0,1] = 1
-matrix[0,2] = 2
-matrix[0,3] = numero_pg - 1
-print("Nueva tabla")
-
-for i in range(1, int(numero_pg/4)):
-    matrix[i, 0] = matrix[i-1, 0] - 2
-    matrix[i, 1] = matrix[i-1, 1] + 2
-    matrix[i, 2] = matrix[i-1, 2] + 2
-    matrix[i, 3] = matrix[i-1, 3] - 2
-
-"""
-
 
 
 matrix[numero_folios_reales-1,2] = (numero_folios_reales*4)/2
 matrix[numero_folios_reales-1,1] = matrix[numero_folios_reales-1,2] -1
 matrix[numero_folios_reales-1,3] = matrix[numero_folios_reales-1,2] +1
 matrix[numero_folios_reales-1,0] = matrix[numero_folios_reales-1,3] +1
-print("Nueva tabla")
+
 
 for i in range(int(numero_folios_reales)-2, -1, -1):
-    print(i)
+    #print(i)
     matrix[i, 0] = matrix[i+1, 0] + 2
     matrix[i, 1] = matrix[i+1, 1] - 2
     matrix[i, 2] = matrix[i+1, 2] - 2
     matrix[i, 3] = matrix[i+1, 3] + 2
 
-#print(matrix[2-1, 0])
+print("Matriz definitiva")
 print(matrix)
 
+#######################################################################
+#Merge pdf
 
-"""
-numero_pg = 16
+pdf_page = "jose"
+print("Desobediencia_civil_%s.pdf" % pdf_page)
+lista_pdfs_cara_A = []
 
-def numero_carillas(numero_pg):
-    if numero_pg % 2 == 0:
-        numero_carillas = int(numero_pg /2)
-        print("Es par")
-    else:
-        numero_carillas = int(numero_pg /2) +1
-        print("No es par")
-    return numero_carillas
+for i in range(0, numero_folios_reales):
+        lista_pdfs_cara_A.append("Desobediencia_civil_%s.pdf" % matrix[i,0])
+        lista_pdfs_cara_A.append("Desobediencia_civil_%s.pdf" % matrix[i, 1])
 
-def numero_folios(numero_carillas):
-    if numero_carillas % 2 == 0:
-        numero_folios = int(numero_carillas / 2)
-    else:
-        numero_folios = int(numero_carillas / 2) + 1
-    return numero_folios
+print(lista_pdfs_cara_A)
+#pdfs = ["Desobediencia_civil_%s.pdf" % name]
 
-print("Número de carillas: ")
-print(numero_carillas(numero_pg))
+merger = PdfFileMerger()
 
-print("Número de folios: ")
-print(numero_folios(numero_carillas(numero_pg)))
+for pdf in lista_pdfs_cara_A:
+    merger.append(pdf)
 
-
-matriz = np.arange(1,((numero_folios(numero_carillas(numero_pg))* 4)+1)).reshape((numero_folios(numero_carillas(numero_pg)), 4))
-print(matriz)
-"""
-# Reemplazamiento de páginas:
-"""def pagina_central(numero_pg):
-    if numero_pg % 2 == 0:
-        pagina_central = int(numero_pg / 2)
-    else:
-        pagina_central = int(numero_pg / 2) + 1
-    return pagina_central
-"""
-"""print("Página central: ")
-pagina_centro = pagina_central(numero_pg)
-print("Página centro")
-print(pagina_centro)
-n_positivo = int(numero_pg / 2)
-n_negativo = -int(numero_pg / 2)
-print("N_negativo")
-print(n_negativo)
-print("N_positivo")
-print(n_positivo)
-n = pagina_centro
-numero_mayor = numero_folios(numero_carillas(numero_pg)) * 4
-print("Número mayor: ")
-print(numero_mayor)
-print(n)
-
-
-# Posición central
-print("Posición central: ")
-posicion_central = numero_mayor -2
-print(posicion_central)
-
-# Valor central
-valor_central = numero_mayor /2
-# Modificación del valor de la posición central:
-"""
-"""for x in horquilla:
-    posicion_central = 0
-    valor_central = 0
-    posicion_central = posicion_central + x
-    valor_central = valor_central + x
-    print(posicion_central)
-    matriz.put(posicion_central, valor_central)
-    posicion_central = posicion_central - x
-    valor_central = valor_central - x
-    #matriz.put(posicion_central, valor_central)"""
-"""
-for x in range(1,n+1):
-    print("La x vale: ")
-    print(x)
-    a = 1
-    b = numero_mayor
-    #print(int(a*x), int(b*x))
-    print(int(a+a*x), int(b-a*x))
-
-    #np.place(matriz, matriz == (numero_mayor-x+1), numero_mayor-x)
-    np.place(matriz, matriz == numero_mayor-1, n)
-    np.place(matriz, matriz == numero_mayor - 1+x, n+x)
-    np.place(matriz, matriz == numero_mayor - 1-x, n - x)
-"""
-"""
-print("Nuevo Esquema: ")
-print(matriz)
-
-numero_mayor = 16
-numero_menor = 1
-
-lista = (1,3,1,3,1,3)
-def ascension(numero_mayor, numero_menor):
-    posicion_inicio = 2
-    posicion_final = 1
-    np.place(matriz, matriz == posicion_final, numero_mayor)
-    np.place(matriz, matriz == posicion_inicio, numero_menor)
-    for x in lista:
-        for i in range(numero_mayor):
-            np.place(matriz, matriz == posicion_inicio+i, numero_menor+1)
-            print(i)
-            posicion_inicio = posicion_inicio + x
-        return posicion_inicio
-
-#posicion_inicio+lista[x]
-ascension(numero_mayor, numero_menor)
-print(matriz)
-
-print("CODIFICACIÓN")
-codificacion = []
-
-for i in range(int(int(numero_mayor)/2)):
-    codificacion.append(1)
-    codificacion.append(3)
-
-print(codificacion)
-
-
-for x in range(numero_mayor):
-    matriz
-
-# DIVIDIR PDF
-def split(path, name_of_split):
-    pdf = PdfFileReader(path)
-    for page in range(pdf.getNumPages()):
-        pdf_writer = PdfFileWriter()
-        pdf_writer.addPage(pdf.getPage(page))
-
-        output = f'{name_of_split}{page}.pdf'
-        with open(output, 'wb') as output_pdf:
-            pdf_writer.write(output_pdf)
-
-# UNIR PDF
-def merger(output_path, input_paths):
-    pdf_writer = PdfFileWriter()
-    for path in input_paths:
-        pdf_reader = PdfFileReader(path)
-        for page in range(pdf_reader.getNumPages()):
-            pdf_writer.addPage(pdf_reader.getPage(page))
-    with open(output_path, 'wb') as fh:
-        pdf_writer.write(fh)
-"""
-
-
-
-
-
-
-
-
-
-
-
+merger.write("Cara_A.pdf")
+merger.close()
 
 if __name__ == '__main__':
     path = 'Desobediencia_civil.pdf'
-    #extract_information(path)
+    extract_information(path)
 
     #split(path, "Desobediencia_civil_")
 
-    #paths = glob.glob('w9_*.pdf')
-    #paths.sort()
-    #merger('pdf_merger.pdf', paths)
+    paths = glob.glob('w9_*.pdf')
+    paths.sort()
+    merger('pdf_merger.pdf', paths)

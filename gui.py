8a89b4d8342e0e15fe2b4c60b9c8a98427367661
fraluegut <franciscojavierluengo@outlook.com>
2020-05-23 13:18:53 +0200

    Avances en TK

--- gui.py
+++ gui.py
@@ -5,21 +5,22 @@ from main import extract_information
 import tkinter as tk
 window = Tk()
 from PyPDF2 import PdfFileReader, PdfFileMerger, PdfFileWriter
+
+import math
+
 window.title("PDF to A5 Printable Book")
-
-
-
 window.filename = None
 def clicked():
     window.filename = filedialog.askopenfilename(initialdir="/home/fraluegut/Descargas", title="Select file", filetypes=(("all files", "*.*"), ("jpeg files", "*.jpg")))
 
     with open(window.filename, 'rb') as f:
         pdf = PdfFileReader(f)
-        result = pdf.getNumPages()
-    numero_paginas_result.configure(text=result)
+        numero_pg = pdf.getNumPages()
+    numero_paginas_result.configure(text=numero_pg)
+    numero_folios_result.configure(text=str(math.ceil(numero_pg/4)))
     print(window.filename)
 
-    return window.filename
+    return window.filename, numero_pg
 
 # Display
 
@@ -28,6 +29,7 @@ btn.grid(column=1, row=0)
 
 nombre_salida = tk.Entry(window) # Nombre de salida
 nombre_salida.grid(column=1, row=1)
+#nombre_salida.place(relx=0.0, rely=0.0, anchor=NW)
 
 ubicacion_salida = Button(window, text="Ubicación de salida", command=clicked) # Botón de Ubicación de salida
 ubicacion_salida.grid(column=1, row=2)
@@ -39,8 +41,8 @@ numero_paginas_result.grid(column=1, row=4)
 
 numero_folios = Label(window, text="Número de folios necesarios: ") # Número de folios necesarios
 numero_folios.grid(column=1, row=5)
-numero_paginas_result = Label(window, text="")
-numero_paginas_result.grid(column=1, row=6)
+numero_folios_result = Label(window, text="")
+numero_folios_result.grid(column=1, row=6)
 
 if window.filename is not None:
     a = window.filename
